eb6f2b55682deb9215efae47f4bd8717c7119c73
mattbrailsford <matt@theoutfield.net>
2013-06-12 11:05:43 +0100

    Initial code import

---
+++ README.md
@@ -0,0 +1,12 @@
+# ezSearch
+ezSearch is the easy search solution for your Umbraco MVC websites. It's fast, self contained and completely configurable.
+
+With ezSearch you can search for words and phrases in your content and media and have the results displayed in a paged list sorted by relevance and with search term highlighting.
+
+ezSearch runs on Umbraco 6.0.4+ and is powered by the built in Lucene index and razor, giving you a fast yet flexible site search package.
+
+<http://our.umbraco.org/projects/website-utilities/ezSearch>
+
+### License
+This project is licensed under [MIT License](http://opensource.org/licenses/mit-license).
+Please see LICENSE.txt for further details.
---
+++ Src/Our.Umbraco.ezSearch/PackageActions/RebuildIndexPackageAction.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Examine;
+using umbraco.cms.businesslogic.packager.standardPackageActions;
+using umbraco.interfaces;
+using umbraco.BusinessLogic;
+
+namespace Our.Umbraco.ezSearch.PackageActions
+{
+    public class RebuildIndexPackageAction : IPackageAction
+    {
+        public string Alias()
+        {
+            return "RebuildIndex";
+        }
+
+        public bool Execute(string packageName, XmlNode xmlData)
+        {
+            if (xmlData == null || xmlData.Attributes == null)
+                return false;
+
+            var indexAttr = xmlData.Attributes["index"];
+            if (indexAttr == null)
+                return false;
+
+            var index = xmlData.Attributes["index"].InnerText;
+            if (string.IsNullOrEmpty(index))
+                return false;
+
+            if (ExamineManager.Instance.IndexProviderCollection.All(x => x.Name != index))
+                return false;
+
+            var indexer = ExamineManager.Instance.IndexProviderCollection[index];
+            indexer.RebuildIndex();
+            return true;
+        }
+
+        public bool Undo(string packageName, XmlNode xmlData)
+        {
+            return true;
+        }
+
+        public XmlNode SampleXml()
+        {
+            var sample = "<Action runat=\"install\" undo=\"true\" alias=\"RebuildIndex\" index=\"IndexName\" />";
+            return helper.parseStringToXmlNode(sample);
+        }
+    }
+}
